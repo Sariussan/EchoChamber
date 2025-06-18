513bbeafd497f756e5cf05378aa8dc68016ec2a6
Simon Weißenbach <simonweissenbach@gmx.de>
2025-06-18 12:23:14 +0200

    echo chamber added

---
+++ Raspbi/echo_chamber.py
@@ -0,0 +1,17 @@
+# Background loop - Inactive loop
+
+# Passive loop
+
+# Activate on input from arduino
+
+# listen for voice
+
+# save what was said
+
+# build prompt
+
+# generate response
+
+# play response
+
+# Reset to inactive state 
